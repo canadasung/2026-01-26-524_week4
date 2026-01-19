7e338b035b9427a7df64df7306dca39a2d6b2d0c
Daniel Chen <chendaniely@users.noreply.github.com>
2026-01-19 10:34:30 -0800

    fix bug

--- src/pytest1/countchar/count_char.py
+++ src/pytest1/countchar/count_char.py
@@ -8,7 +8,7 @@ def count_char(input_string):
         raise TypeError(f"Expected the input to be of type str, got {type(input_string)}")
 
     #return len(input_string)
-    count = 1
+    count = 0
     for char in input_string:
         count += 1
     return count
